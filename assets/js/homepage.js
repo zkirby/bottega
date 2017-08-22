0b239755219b77f060136f97d4e3de04988843d5
Zachary Kirby <ARK@airbears2-10-142-172-103.airbears2.1918.berkeley.edu>
2017-08-22 16:46:16 -0700

    about section

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 	$nav_show = $("#nav-show");
 
 	$(".hamburger").click(function() {
-		console.log($nav_show.css("left"));
 		if ($(this).hasClass("is-active")) {
 			$(this).removeClass("is-active");
 			$nav_show.css("left", "-115%");
@@ -12,6 +11,20 @@ $(document).ready(function() {
 			$nav_show.css("left", "-4%");
 		}
 	}) 
+
+	$(".overlay").click(function() {
+		if ($(".hamburger").hasClass("is-active")) {
+			$(".hamburger").removeClass("is-active");
+			$nav_show.css("left", "-115%");
+		} else {
+			$(".hamburger").addClass("is-active");
+			$nav_show.css("left", "-4%");
+		}
+	}) 
+
+	$(".about").click(function() {
+		zenscroll.to(document.getElementsByClassName("about-container")[0]);
+	});
 
 
 });
