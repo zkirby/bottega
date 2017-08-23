332973c006409cf163b22211716fe309381465d0
Zachary Kirby <ARK@airbears2-10-142-172-171.airbears2.1918.berkeley.edu>
2017-08-23 16:19:15 -0700

    design improvments

--- homepage.js
+++ homepage.js
@@ -1,28 +1,24 @@
 $(document).ready(function() {
 
-	// Pages
-	var $landing = $(".landing-body");
-	var $about = $(".about-body");
-	var $events = $(".events-body")
-
 	// Current View
-	var current_page = $landing;
-
-	// Pages Dict
-	var pages = { "about":$about, "events":$events, "apply":$events };
-
+	var current_page = $(".landing-body");;
 
 	$(".nav-item").click(function() {
+
+		// Grab the page name and activate the page
 		let page = this.className.slice(8, this.className.length);
-		current_page = activate(pages[page.trim()], current_page);
+		current_page = activate(page.trim(), current_page);
 	})
 
 });
 
-
+// Function swaps displays on two pages
 activate = (page, current_page) => {
 	current_page.css("display", "none");
-	page.css("display", "block");
 
-	return page;
+	let active_page = $("." + page + "-body");
+
+	active_page.css("display", "block");
+
+	return active_page;
 };
