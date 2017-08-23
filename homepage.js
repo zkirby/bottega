bc96d90d8cf43b0761f8d394daab5c59fc90c1bf
Zachary Kirby <ARK@airbears2-10-142-172-171.airbears2.1918.berkeley.edu>
2017-08-23 14:51:41 -0700

    added page switching

--- homepage.js
+++ homepage.js
@@ -3,17 +3,18 @@ $(document).ready(function() {
 	// Pages
 	var $landing = $(".landing-body");
 	var $about = $(".about-body");
+	var $events = $(".events-body")
 
+	// Current View
 	var current_page = $landing;
 
-
 	// Pages Dict
-	var pages = { "about":$about };
+	var pages = { "about":$about, "events":$events, "apply":$events };
 
 
 	$(".nav-item").click(function() {
 		let page = this.className.slice(8, this.className.length);
-		activate(pages[page.trim()], current_page);
+		current_page = activate(pages[page.trim()], current_page);
 	})
 
 });
@@ -23,5 +24,5 @@ activate = (page, current_page) => {
 	current_page.css("display", "none");
 	page.css("display", "block");
 
-	current_page = page;
+	return page;
 };
